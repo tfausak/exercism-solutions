d7121582e3e36e811d75dc6bb144856ee9d08686
Taylor Fausak <taylor@fausak.me>
2013-08-29 18:14:02 -0500

    Clean up Phone module
    Lots of good changes:
    
    -   Return a (String, String, String) instead of (String, (String,
        String)) from parts and use pattern matching to extract values from
        it.
    -   Use guard clauses instead of nested conditionals.

--- haskell/phone-number/Phone.hs
+++ haskell/phone-number/Phone.hs
@@ -3,25 +3,21 @@ module Phone (areaCode, number, prettyPrint) where
 import Data.Char (isDigit)
 
 areaCode :: String -> String
-areaCode s = fst $ parts s
+areaCode s = a
+  where (a, _, _) = parts s
 
 number :: String -> String
-number s =
-  let s' = filter isDigit s
-  in
-    if length s' == 10
-    then s'
-    else
-      if length s' == 11 && head s' == '1'
-      then tail s'
-      else "0000000000"
+number s
+  | length s' == 10 = s'
+  | length s' == 11 && head s' == '1' = tail s'
+  | otherwise = "0000000000"
+  where s' = filter isDigit s
 
 prettyPrint :: String -> String
-prettyPrint s =
-  let s' = parts s
-  in concat ["(", fst s', ") ", fst (snd s'), "-", snd (snd s')]
+prettyPrint s = concat ["(", a, ") ", b, "-", c]
+  where (a, b, c) = parts s
 
-parts :: String -> (String, (String, String))
-parts s =
-  let s' = number s
-  in (take 3 s', splitAt 3 (drop 3 s'))
+parts :: String -> (String, String, String)
+parts s = (a, b, c)
+  where (a, bc) = splitAt 3 $ number s
+        (b, c) = splitAt 3 bc
