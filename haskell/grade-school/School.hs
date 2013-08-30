3a529dd2f20a561dd42cd93ba315ee5db73ad14a
Taylor Fausak <taylor@fausak.me>
2013-08-29 20:37:19 -0500

    Add solution to phone-number

--- haskell/grade-school/School.hs
+++ haskell/grade-school/School.hs
@@ -6,18 +6,27 @@ module School
 , sorted
 ) where
 
+import qualified Data.Map as Map
+import Data.List (sort)
+
 type School = [(Grade, [Name])]
 type Grade = Int
 type Name = String
 
 add :: Grade -> Name -> School -> School
-add g n s = empty
+add g n s = Map.toList s'' where
+  s'' = Map.insertWith (++) g [n] s'
+  s' = Map.fromList s
 
 empty :: School
 empty = []
 
 grade :: Grade -> School -> [String]
-grade g s = empty
+grade g s = Map.findWithDefault [] g s' where
+  s' = Map.fromList s
 
 sorted :: School -> School
-sorted s = empty
+sorted s = map f s'' where
+  f (g, ns) = (g, sort ns)
+  s'' = Map.toAscList s'
+  s' = Map.fromList s
