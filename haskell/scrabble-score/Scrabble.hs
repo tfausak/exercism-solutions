dab88636899b70305b43192b22c9725bde1ee3a5
Taylor Fausak <taylor@fausak.me>
2013-09-19 08:55:05 -0500

    Use a Map instead of repeated checks

--- haskell/scrabble-score/Scrabble.hs
+++ haskell/scrabble-score/Scrabble.hs
@@ -4,17 +4,20 @@ module Scrabble
 ) where
 
 import Data.Char (toLower)
+import Data.Map (findWithDefault, fromList)
 
 scoreLetter :: Char -> Int
-scoreLetter letter
-    | isIn "dg"    = 2
-    | isIn "bcmp"  = 3
-    | isIn "fhvwy" = 4
-    | isIn "k"     = 5
-    | isIn "jx"    = 8
-    | isIn "qz"    = 10
-    | otherwise    = 1
-    where isIn x = toLower letter `elem` x
+scoreLetter letter = findWithDefault 0 (toLower letter) scores where
+    scores = fromList $ concat
+        [ repeatZip 1 "aeilnorstu"
+        , repeatZip 2 "dg"
+        , repeatZip 3 "bcmp"
+        , repeatZip 4 "fhvwy"
+        , repeatZip 5 "k"
+        , repeatZip 8 "jx"
+        , repeatZip 10 "qz"
+        ]
+    repeatZip score letters = zip letters (repeat score)
 
 scoreWord :: String -> Int
 scoreWord letters = sum $ map scoreLetter letters
