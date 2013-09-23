843634b0b8dc105ba4e883f02d805536271ec796
Taylor Fausak <taylor@fausak.me>
2013-09-22 20:35:51 -0500

    Change solution to use foldl'

--- haskell/binary/Binary.hs
+++ haskell/binary/Binary.hs
@@ -2,17 +2,13 @@ module Binary
 ( toDecimal
 ) where
 
-import Data.Map (findWithDefault, fromList)
+import Data.List (foldl')
 
 toDecimal :: String -> Int
-toDecimal = convert "01"
+toDecimal = convert fromBinary 2 where
+    fromBinary = fromEnum . ('1' ==)
 
-convert :: String -> String -> Int
-convert digits string = sum parts where
-    radix = length digits
-    digitValues = fromList $ zip digits [0..]
-    values = map getValue string
-    getValue digit = findWithDefault 0 digit digitValues
-    pairs = zip (reverse values) [0..]
-    parts = map exponentiate pairs
-    exponentiate (value, position) = value * radix ^ position
+convert :: (Char -> Int) -> Int -> String -> Int
+convert fromBase radix string = foldl' accumulate 0 values where
+    values = map fromBase string
+    accumulate number digit = number * radix + digit
