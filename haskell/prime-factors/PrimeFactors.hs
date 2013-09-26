692c764f3d1c4cdef438e2cca8cb9145aa57deb3
Taylor Fausak <taylor@fausak.me>
2013-09-26 09:17:19 -0500

    Drastically reduce the search space
    Don't check all potential divisors. Instead, only check 2, 3, and
    integers of the form 6k+/-1. They're the only ones that can be prime
    anyway.

--- haskell/prime-factors/PrimeFactors.hs
+++ haskell/prime-factors/PrimeFactors.hs
@@ -3,11 +3,14 @@ module PrimeFactors
 ) where
 
 primeFactors :: (Integral a) => a -> [a]
-primeFactors = f 2 where
-    f d n
-        | n <= 1 || n < d = []
+primeFactors = f divisors where
+    divisors = 2 : 3 : [x + y | x <- [5, 11 ..], y <- [0, 2]]
+    f ds n
+        | n < d = []
         | otherwise = if r == 0
-            then d : f d q
-            else f (d + 1) n
-            where
-                (q, r) = quotRem n d
+            then d : f ds q
+            else f ds' n
+        where
+            d = head ds
+            ds' = tail ds
+            (q, r) = quotRem n d
