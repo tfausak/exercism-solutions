255b3f3dff77cc649d2c7dd215163923670b4114
Taylor Fausak <taylor@fausak.me>
2013-09-29 19:05:02 +0000

    Add Haskell solution to allergies

---
+++ haskell/allergies/Allergies.hs
@@ -0,0 +1,25 @@
+module Allergies
+( Allergen(..)
+, allergies
+, isAllergicTo
+) where
+
+import Data.Bits (testBit)
+
+data Allergen = Eggs
+              | Peanuts
+              | Shellfish
+              | Strawberries
+              | Tomatoes
+              | Chocolate
+              | Pollen
+              | Cats
+              deriving (Bounded, Enum, Eq, Show)
+
+allergies :: Int -> [Allergen]
+allergies score = filter isAllergicTo' allergens where
+    isAllergicTo' = flip isAllergicTo score
+    allergens = enumFrom minBound
+
+isAllergicTo :: Allergen -> Int -> Bool
+isAllergicTo allergen score = testBit score $ fromEnum allergen
