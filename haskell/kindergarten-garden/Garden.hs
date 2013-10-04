4bccc844d018e24f9e865719e9d5c1702ae12361
Taylor Fausak <taylor@fausak.me>
2013-10-04 13:52:50 -0500

    Clean up solution

--- haskell/kindergarten-garden/Garden.hs
+++ haskell/kindergarten-garden/Garden.hs
@@ -5,8 +5,9 @@ module Garden
 , lookupPlants
 ) where
 
-import Data.List (sort)
-import Data.Map (Map, findWithDefault, fromListWith)
+import Data.List (sort, transpose)
+import Data.List.Split (chunksOf)
+import Data.Map (Map, findWithDefault, fromList)
 
 data Plant = Clover
            | Grass
@@ -33,18 +34,13 @@ defaultGarden = garden
     ]
 
 garden :: [String] -> String -> Garden
-garden students grid =
-    fromListWith
-        (flip (++))
-        [(student, [plant (row !! (index + offset))]) |
-            row <- lines grid,
-            (index, student) <- zip [0,2..] (sort students),
-            offset <- [0, 1]] where
-                plant c
-                    | c == 'C' = Clover
-                    | c == 'G' = Grass
-                    | c == 'R' = Radishes
-                    | c == 'V' = Violets
+garden students grid = fromList (zip (sort students) (transform grid)) where
+    transform = map (map plant . concat) . transpose . map (chunksOf 2) . lines
+    plant letter
+        | letter == 'C' = Clover
+        | letter == 'G' = Grass
+        | letter == 'R' = Radishes
+        | letter == 'V' = Violets
 
 lookupPlants :: String -> Garden -> [Plant]
 lookupPlants = findWithDefault []
