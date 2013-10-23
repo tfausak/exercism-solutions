3c320c27b455decd9292f63010ccc39439a83597
Taylor Fausak <taylor@fausak.me>
2013-10-23 12:18:28 -0500

    Add Haskell solution to matrix

--- haskell/matrix/Matrix.hs
+++ haskell/matrix/Matrix.hs
@@ -12,37 +12,46 @@ module Matrix
 , transpose
 ) where
 
-import Data.Vector (Vector)
+import qualified Data.Vector as V
 
-data Matrix a = Matrix a
-    deriving (Eq, Show)
+type Matrix a = V.Vector (V.Vector a)
+
+chunks :: Int -> V.Vector a -> V.Vector (V.Vector a)
+chunks n xs
+    | n < 1 || V.null xs = V.empty
+    | otherwise = h `V.cons` chunks n t where
+        (h, t) = V.splitAt n xs
 
 cols :: Matrix a -> Int
-cols _ = undefined
+cols m
+    | V.null m = 0
+    | otherwise = V.length (V.head m)
 
-column :: Int -> Matrix a -> Vector a
-column _ _ = undefined
+column :: Int -> Matrix a -> V.Vector a
+column = V.map . flip (V.!)
 
-flatten :: Matrix a -> Vector a
-flatten _ = undefined
+flatten :: Matrix a -> V.Vector a
+flatten = V.concat . V.toList
 
 fromList :: [[a]] -> Matrix a
-fromList _ = undefined
+fromList = V.fromList . map V.fromList
 
-fromString :: String -> Matrix a
-fromString _ = undefined
+fromString :: Read a => String -> Matrix a
+fromString s = fromList $ map f $ lines s where
+    f l = if null r then [] else fst (head r) : f (snd (head r)) where
+        r = reads l
 
 reshape :: (Int, Int) -> Matrix a -> Matrix a
-reshape _ _ = undefined
+reshape (_, y) m = chunks y $ flatten m
 
-row :: Int -> Matrix a -> Vector a
-row _ _ = undefined
+row :: Int -> Matrix a -> V.Vector a
+row = flip (V.!)
 
 rows :: Matrix a -> Int
-rows _ = undefined
+rows = V.length
 
 shape :: Matrix a -> (Int, Int)
-shape _ = undefined
+shape m = (rows m, cols m)
 
 transpose :: Matrix a -> Matrix a
-transpose _ = undefined
+transpose m = V.fromList $ map (`column` m) [0 .. cols m - 1]
