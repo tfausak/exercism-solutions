3edb1b7b5e13dc0e5cb70e75478f0b1d51b4e5dc
Taylor Fausak <taylor@fausak.me>
2013-10-28 20:29:42 -0500

    Use a strict fold

--- haskell/octal/Octal.hs
+++ haskell/octal/Octal.hs
@@ -3,12 +3,14 @@ module Octal
 , showOct
 ) where
 
+import Data.List (foldl')
+
 readOct :: (Integral a) => String -> a
-readOct = foldl f 0 where
-    f n c = n * 8 + (fromIntegral (fromEnum c - 48))
+readOct = foldl' f 0 where
+  f n x = n * 8 + fromIntegral (fromEnum x - 48)
 
 showOct :: (Integral a) => a -> String
 showOct = reverse . showOct' where
     showOct' 0 = ""
-    showOct' n = toEnum ((fromIntegral r) + 48) : showOct' q where
+    showOct' n = toEnum (fromIntegral r + 48) : showOct' q where
         (q, r) = n `quotRem` 8
